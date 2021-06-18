12afcf2965634ac7d0637c962c96e1297de58950
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-06-18 03:37:02 +0300

    Add some docs

--- src/main/kotlin/juuxel/accesswidener/idea/psi/impl/AwReferencingElementImpl.kt
+++ src/main/kotlin/juuxel/accesswidener/idea/psi/impl/AwReferencingElementImpl.kt
@@ -5,6 +5,10 @@ import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiReference
 import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
 
+/**
+ * Provides reference searching support.
+ * Must be mixed into all referencing elements.
+ */
 abstract class AwReferencingElementImpl(node: ASTNode) : ASTWrapperPsiElement(node) {
     override fun getReference(): PsiReference? = references.firstOrNull()
 
