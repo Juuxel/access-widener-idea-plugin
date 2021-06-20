1baa1ddda47ab65528e0086d8efd7810dd4ded52
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-06-20 17:13:47 +0300

    Add inspection for unknown target

---
+++ src/main/kotlin/juuxel/accesswidener/idea/inspection/UnknownTargetInspection.kt
@@ -0,0 +1,28 @@
+package juuxel.accesswidener.idea.inspection
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiPolyVariantReference
+import juuxel.accesswidener.idea.psi.AwDefinition
+import juuxel.accesswidener.idea.psi.AwVisitor
+
+class UnknownTargetInspection : LocalInspectionTool() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : AwVisitor() {
+            override fun visitDefinition(o: AwDefinition) {
+                for (ref in o.references) {
+                    val missing = if (ref is PsiPolyVariantReference) {
+                        ref.multiResolve(false).isEmpty()
+                    } else {
+                        ref.resolve() == null
+                    }
+
+                    if (missing) {
+                        holder.registerProblem(ref)
+                    }
+                }
+            }
+        }
+    }
+}
